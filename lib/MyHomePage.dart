173e1a212e6efe2b0bff9780406dc205dbb13bb8
kim <alongthecloud@gmail.com>
2023-11-29 16:30:01 +0900

    check application state event using WidgetsBindingObserver

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -14,19 +14,62 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+  ViewStateProvider? _viewStateProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    var logger = SimpleLogger();
+
+    switch (state) {
+      case AppLifecycleState.resumed:
+        setState(() {});
+        logger.info('resumed');
+        break;
+      case AppLifecycleState.inactive:
+        if (_viewStateProvider != null) {
+          var viewState = _viewStateProvider!.viewerState;
+          logger.info("opened image: ${viewState.curImagePath}");
+        }
+        logger.info('inactive');
+        break;
+      case AppLifecycleState.detached:
+        logger.info('detached');
+        // DO SOMETHING!
+        break;
+      case AppLifecycleState.paused:
+        logger.info('paused');
+        break;
+      default:
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     var logger = SimpleLogger();
     logger.info("run MyHomePage.build");
 
-    return ChangeNotifierProvider(
-        create: (context) => ViewStateProvider(),
-        builder: (context, child) {
-          return Scaffold(
-              body: OKToast(
-                  child: Container(
-                      color: Colors.grey, child: const MyDropRegion())));
-        });
+    return ChangeNotifierProvider(create: (context) {
+      _viewStateProvider ??= ViewStateProvider();
+      return _viewStateProvider;
+    }, builder: (context, child) {
+      return Scaffold(
+          body: OKToast(
+              child:
+                  Container(color: Colors.grey, child: const MyDropRegion())));
+    });
   }
 }
