98b9aa7ed1cd1919fca43fb152a58e1cdc813e90
kim <alongthecloud@gmail.com>
2023-12-01 16:47:33 +0900

    change to Responsive layout

--- lib/view/ImagePropertyView.dart
+++ lib/view/ImagePropertyView.dart
@@ -1,19 +1,11 @@
 import 'dart:ui';
-import 'dart:io';
-import 'package:path/path.dart' as path;
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:sdimage_viewer/provider/AppConfigProvider.dart';
 import 'package:simple_logger/simple_logger.dart';
 import 'package:expandable_text/expandable_text.dart';
-import '../model/AppPath.dart';
 import '../model/ViewerState.dart';
 import '../util/WidgetUtil.dart';
 import '../util/Util.dart';
-import '../util/ImageUtil.dart';
 import '../util/MetaKeyword.dart';
-import './HelpView.dart';
-import './SettingsView.dart';
 
 class ImagePropertyView extends StatelessWidget {
   final ViewerState viewerState;
@@ -78,98 +70,6 @@ class ImagePropertyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var metaTable = viewerState.curImageMetaData.getMetaTable();
-    var appConfigProvider =
-        Provider.of<AppConfigProvider>(context, listen: false);
-    var appConfig = appConfigProvider.appConfig;
-    var appUserData = appConfig.appUserData;
-
-    getOffsetAndPath() {
-      // calc offset
-      var currentImage = viewerState.curImageData;
-      Offset offset = Offset.zero;
-      if (appUserData.waterMarkImage != null && currentImage != null) {
-        var watermarkconfig = appConfig.watermark;
-        if (watermarkconfig != null) {
-          var wmImage = appUserData.waterMarkImage!;
-          var margin = appConfig.watermark!.margin.toDouble();
-
-          offset = ImageUtil.calcAlignmentOffset(
-              appConfig.watermark!.alignment,
-              Size(wmImage.width.toDouble(), wmImage.height.toDouble()),
-              Size(currentImage.width.toDouble(),
-                  currentImage.height.toDouble()),
-              Offset(margin, margin));
-        }
-      }
-
-      String prefix = '';
-      if (appConfig.general != null) {
-        prefix = appConfig.general!.savefileprefix;
-      }
-
-      var appPath = AppPath();
-
-      String targetPath = ImageUtil.getImageFullPath(
-          appPath.outputDirPath, viewerState.curImagePath, prefix);
-
-      return (offset, targetPath);
-    }
-
-    Widget rightButtonBar = ButtonBar(children: [
-      IconButton(
-          icon: const Icon(Icons.save),
-          onPressed: () {
-            var offsetAndPath = getOffsetAndPath();
-            var offset = offsetAndPath.$1;
-            var targetPath = offsetAndPath.$2;
-
-            ImageUtil.saveImageWithWatermark(
-                    targetPath,
-                    viewerState.curImageData,
-                    appUserData.waterMarkImage,
-                    offset)
-                .then((result) async {
-              if (result == true) {
-                Util.showToastMessage(
-                    context, "Image saved !", const Duration(seconds: 1));
-
-                if (appConfig.general != null &&
-                    appConfig.general!.savewithmetatext) {
-                  StringBuffer sb = StringBuffer();
-                  sb.writeln("original file : ${viewerState.curImagePath}");
-
-                  File f = File(viewerState.curImagePath);
-                  // bool exist = f.existsSync();
-                  var modifiedTime = await f.lastModified();
-                  sb.writeln("modify datetime : ${modifiedTime.toString()}");
-                  sb.writeln("meta : ${metaTable.toString()}");
-
-                  var metaFilePath =
-                      "${path.withoutExtension(targetPath)}.meta.txt";
-
-                  Util.saveTextFile(sb.toString(), metaFilePath);
-                }
-              }
-            });
-          }),
-      IconButton(
-          icon: const Icon(Icons.settings),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return SettingsView(appConfig: appConfigProvider.appConfig);
-            })).then((value) {
-              appConfigProvider.save();
-              appConfigProvider.update();
-              return null;
-            });
-          }),
-      IconButton(
-          icon: const Icon(Icons.help),
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const HelpView()));
-          }),
-    ]);
 
     List<Widget> sideItems = [];
     if (viewerState.curImageData != null) {
@@ -199,16 +99,8 @@ class ImagePropertyView extends StatelessWidget {
       sideItems.add(metaTableWidget);
     }
 
-    return Container(
-      padding: const EdgeInsets.fromLTRB(1, 10, 3, 10),
-      width: 380,
-      child: Column(children: [
-        rightButtonBar,
-        const SizedBox(height: 4),
-        Expanded(
-          child: ListView(shrinkWrap: true, children: sideItems),
-        ),
-      ]),
+    return Expanded(
+      child: ListView(shrinkWrap: true, children: sideItems),
     );
   }
 }
